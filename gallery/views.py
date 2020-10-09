2ea71320856bf211cd1020d67755da7d45068abf
Nyururukelvin <nyururukelvin9@gmail.com>
2020-10-09 15:45:06 +0300

    my commit add templates & migrations

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
+# Views.
+def home(request):
+    return render(request, 'galleries/gallery.html')
