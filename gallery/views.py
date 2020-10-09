e4f2761346da1aa8038ce0d77b142316df7ff2b3
Nyururukelvin <nyururukelvin9@gmail.com>
2020-10-09 20:57:55 +0300

    my commit add first picture

--- gallery/views.py
+++ gallery/views.py
@@ -11,11 +11,10 @@ def home(request):
 def search_category(request):
     if 'category' in request.GET and request.GET['category']:
         search_term=request.GET.get('category')
-        searched_category=Image.search_image(search_term)
-        print(searched_category)
+        images =Image.search_image(search_term)
         message=f'{search_term}'
         
-        return render(request,'galleries/search_category.html',{'message':message,'searched':searched_category,'title':search_term})
+        return render(request,'galleries/search_category.html',{'message':message,'images':images,'title':search_term})
     
     else:
         message=f"You haven't searched for anything."
