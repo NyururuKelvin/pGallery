4155bb94cb15a93231e64e12f5cfb1e5bebf7134
Nyururukelvin <nyururukelvin9@gmail.com>
2020-10-09 21:22:35 +0300

    my commit add methods

--- gallery/models.py
+++ gallery/models.py
@@ -13,10 +13,64 @@ class Image(models.Model):
     # Saving the image
     def save_image(self):
         self.save()
+    
+    # Updating the image
+    @classmethod
+    def update_image(cls,name,update):
+        Image.objects.filter(image_name=name).update(image_name=update)
+        update=Image.objects.get(image_name=update)
+        return update    
+        
+    # Deleting the image    
+    @classmethod    
+    def delete_image(cls,image):
+        Image.objects.get(image_name=image).delete()
+        
+    # Getting image by ID
+    @classmethod
+    def get_image_by_id(cls,id):
+        image=Image.objects.get(pk=id)
+        return image
+    
+    # Searching images
+    @classmethod
+    def search_image(cls,category_image):
+        categories=Category.objects.filter(category=category_image)
+        for category in categories:          
+            image=cls.objects.filter(category=category)
+        return image
+
+    # Filtering images by location 
+    @classmethod
+    def filter_by_location(cls,image_location):
+        locations=Location.objects.filter(location=image_location)
+        for location in locations:
+            images=Image.objects.filter(location=location)
+        return images
+    
+    def __str__(self):
+        return self.image_name
 
 class Location(models.Model):
     location=models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.location
+
+    # Updating the location
+    @classmethod
+    def update_location(cls,name,update):
+        Location.objects.filter(location=name).update(location=update)  
+        updated=Location.objects.get(location=update)
+        return updated
+
+        
+    # Deleting the location   
+    @classmethod    
+    def delete_location(cls,location):
+        deleted=Location.objects.get(location=location).delete()
+        return deleted
+    
     def __str__(self):
         return self.location
 
@@ -35,6 +89,18 @@ class Category(models.Model):
     # Saving the category
     def save_category(self):
         self.save()
+
+    # Updating the Category
+    @classmethod
+    def update_category(cls,name,update):
+        Category.objects.filter(category=name).update(category=update)
+        update=Category.objects.get(category=update)
+        return update    
+        
+    # Deleting the image   
+    @classmethod    
+    def delete_category(cls,category):
+        Category.objects.get(category=category).delete()
 
     def __str__(self):
         return self.category
