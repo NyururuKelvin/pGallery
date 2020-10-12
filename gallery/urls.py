8cfb740912958919bcddc90fb190ac893a1f067c
Nyururukelvin <nyururukelvin9@gmail.com>
2020-10-12 16:06:58 +0300

    edit code

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 urlpatterns=[
     url(r'^$', views.home, name ='home'),
-    url(r'^search/', views.search_category, name='search_category')
+    url(r'^search/', views.search_category, name='search_category'),
+    url(r'^location/', views.filter_by_location, name='filter_by_location'),
+    url(r'^image/', views.get_image_id, name='get_image_id'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
